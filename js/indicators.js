e219fbdd5de3309db01992cf01ca0afc57004504
Martin Petruch <78723403@cuni.cz>
2024-08-23 09:37:27 +0200

    dashed lone transitionadded and texts animated

--- js/indicators.js
+++ js/indicators.js
@@ -1,1 +1,21 @@
 // JS for the lower part of website
+const textsNodeArr = document.querySelectorAll(".single-indicator-p");
+const texts = Array.from(textsNodeArr);
+
+function slideDownText(textIndex) {
+    var text = texts[textIndex]
+    if (text.classList.contains("active")) {
+        text.classList.remove("active");
+    } else {
+        text.classList.add("active");
+    }
+}
+
+const textLogo = document.querySelector(".logo-container-p");
+function slideDownTextLogo() {
+    if (textLogo.classList.contains("active")) {
+        textLogo.classList.remove("active");
+    } else {
+        textLogo.classList.add("active");
+    }
+}
