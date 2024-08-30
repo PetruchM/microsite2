87d9bc975186896a6d81404f3a3dfd89f931ea01
Martin Petruch <78723403@cuni.cz>
2024-08-30 08:28:30 +0200

    overlay responsible phone

--- js/indicators.js
+++ js/indicators.js
@@ -43,3 +43,30 @@ function scrollDown(){
         behavior: 'smooth'
     });
 }
+
+function updatePathBasedOnWidth() {
+    const windowWidth = window.innerWidth;
+
+    const linepathElement = document.getElementById('linepath');
+    const maskpathElement = document.getElementById('maskpath');
+
+    const defaultLinePath = "M 0 20 Q 250 52 500 20 T 1000 20";
+    const narrowLinePath = "M 0 20 Q 250 76 500 20 T 1000 20";
+    const defaultMaskPath = "M 0 0.02 Q 0.25 0.04 0.5 0.02 T 1 0.02 L1 -0.001 L0 -0.001 Z";
+    const narrowMaskPath = "M 0 0.02 Q 0.25 0.03 0.5 0.02 T 1 0.02 L1 -0.001 L0 -0.001 Z";
+
+    // Change the path based on the window width
+    if (windowWidth < 1215) {
+        linepathElement.setAttribute('d', narrowLinePath);
+        maskpathElement.setAttribute('d', narrowMaskPath);
+    } else {
+        linepathElement.setAttribute('d', defaultLinePath);
+        maskpathElement.setAttribute('d', defaultMaskPath);
+    }
+}
+
+// Call the function once to set the initial path
+updatePathBasedOnWidth();
+
+// Add an event listener to call the function when the window is resized
+window.addEventListener('resize', updatePathBasedOnWidth);
