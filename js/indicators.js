63dce8c827f0c91e8ac06de44777035076896db0
Martin Petruch <78723403@cuni.cz>
2024-08-30 07:17:38 +0200

    Text sliding and image trasition working

--- js/indicators.js
+++ js/indicators.js
@@ -1,25 +1,30 @@
 // JS for the lower part of website
 const textsNodeArr = document.querySelectorAll(".single-indicator-p");
 const texts = Array.from(textsNodeArr);
+const imgsNodeArr = document.querySelectorAll(".indicator-svg");
+const imgs = Array.from(imgsNodeArr);
 
 
-function slideDownText(textIndex) {
-    var text = texts[textIndex]
-    var imgElement = document.getElementById('indicator1');
-    if (text.classList.contains("active")) {
-        text.classList.remove("active");
-        if (textIndex==0){
-            imgElement.src = 'images/svgs/Lahev 1.svg';
-        }
+function IndicatorActive(Index, imgNormal, imgActive) {
+    var textElement = texts[Index]
+    var imgElement = imgs[Index]
+    if (textElement.classList.contains("active")) {
+        textElement.classList.remove("active");
+        changeImageSourceSmoothly(imgElement,imgNormal);
     } else {
-        text.classList.add("active");
-        if (textIndex==0){
-            imgElement.src = 'images/svgs/Lahev 2.svg';
-        }
+        textElement.classList.add("active");
+        changeImageSourceSmoothly(imgElement,imgActive);
     }
 }
 
-const textLogo = document.querySelector(".logo-container-p");
+function changeImageSourceSmoothly(Element,newSrc) {
+    Element.classList.add('fade-out');
+    setTimeout(function() {
+        Element.src = newSrc;
+        Element.classList.remove('fade-out');
+    }, 500);
+}
+
 function slideDownTextLogo() {
     if (textLogo.classList.contains("active")) {
         textLogo.classList.remove("active");
