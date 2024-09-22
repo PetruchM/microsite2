608c2ff4ff81ab85f2ceb84e056c8afa8cd2870c
Martin Petruch <78723403@cuni.cz>
2024-09-22 16:27:04 +0200

    clean up - useless files named expandable removed

--- js/expandable.js
+++
@@ -1,5 +0,0 @@
-function toggleAdditionalText(button) {
-    var container = button.parentNode;
-    var additionalText = container.querySelector('.additional-text');
-    additionalText.classList.toggle('show');
-}
