a481eb3147d69c48c77705f2c88dbc77257df366
Martin Petruch <78723403@cuni.cz>
2024-12-04 11:18:21 +0100

    carousel box opacity increased

--- js/carousel_box.js
+++ js/carousel_box.js
@@ -62,7 +62,7 @@ function slideRectangle2() {
     var rectangle2 = document.querySelector('.carousel-item.active .box-info2');
     rectangle2.classList.toggle('hide_left');
 	var rec3content = rectangle3.querySelector('.box-info3-content');
-    rec3content.classList.toggle('opacity75');
+    rec3content.classList.toggle('opacity85');
 }
 
 function slideRectangle3() {
@@ -84,5 +84,5 @@ function hideAll() {
     // rectangle3.classList.remove('info_ready2');
     rectangle3.classList.remove('info_active2');
     var rec3content = rectangle3.querySelector('.box-info3-content');
-    rec3content.classList.remove('opacity75');
+    rec3content.classList.remove('opacity85');
 }
