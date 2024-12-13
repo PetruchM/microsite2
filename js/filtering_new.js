706acbdd509edb497e398783f06ce1de97a5cfab
Martin Petruch <78723403@cuni.cz>
2024-12-13 09:27:17 +0100

    vertical pen mobile

--- js/filtering_new.js
+++ js/filtering_new.js
@@ -387,13 +387,13 @@ function updateBackground(){
 
     if (currentOnPc!=onPC){
         onPC=currentOnPc;
-        if (onPC){
-            pagerCarousel.classList.add('vertical');
-            pagerCarousel.classList.remove('horizontal');
-        }else{
-            pagerCarousel.classList.remove('vertical');
-            pagerCarousel.classList.add('horizontal');
-        }
+        // if (onPC){
+        //     pagerCarousel.classList.add('vertical');
+        //     pagerCarousel.classList.remove('horizontal');
+        // }else{
+        //     pagerCarousel.classList.remove('vertical');
+        //     pagerCarousel.classList.add('horizontal');
+        // }
         setFilter(pcFilterButtons[currentFilter%articlesPerFilter],currentFilter);
     }
 }
