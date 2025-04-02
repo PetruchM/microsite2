9fb0ca174275d14ebc5123c061a16df1b3afc1cf
Martin Petruch <78723403@cuni.cz>
2025-04-02 14:06:20 +0200

    carousel buttons rotating on click and third button disabled

--- js/carousel_box.js
+++ js/carousel_box.js
@@ -55,6 +55,8 @@ function slideRectangle1() {
     var wasActive = rectangle2.classList.toggle('info_active1');
     var rec2content = rectangle2.querySelector('.box-info2-content');
     rec2content.classList.toggle('opacity85');
+    var button1 = document.querySelector('.carousel-item.active .slide_butt1');
+    button1.classList.toggle('button_rotate_on_activation');
 
     var rectangle3 = document.querySelector('.carousel-item.active .box-info3');
     rectangle3.classList.toggle('info_ready2');
@@ -68,6 +70,10 @@ function slideRectangle2() {
     rectangle3.classList.toggle('info_active2');
 	var rec3content = rectangle3.querySelector('.box-info3-content');
     rec3content.classList.toggle('opacity85');
+
+    var button2 = document.querySelector('.carousel-item.active .slide_butt2');
+    button2.classList.toggle('button_rotate_on_activation');
+
 }
 
 // function slideRectangleMobile() {
