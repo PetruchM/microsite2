d32ffeea0239e76726263dcaf97a1a6a7b201083
Martin Petruch <78723403@cuni.cz>
2025-04-03 12:27:17 +0200

    trying out new variable for vh on phone

--- js/global.js
+++ js/global.js
@@ -1,9 +1,13 @@
 function updatePcOrMobile(){
     updateBackground();
     updatePathBasedOnWidth();
+    setVh();
     // updateCarouselCardsFunctions();
 }
-
+function setVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 updatePcOrMobile();
 
 window.addEventListener('resize', updatePcOrMobile);
