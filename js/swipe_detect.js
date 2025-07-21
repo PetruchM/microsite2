c2390e49a5f2f13c2fce0adbbdd15137848cd9dc
Martin Petruch <78723403@cuni.cz>
2025-07-21 21:44:06 +0200

    trying to prevent moving the whole div when swiping on phone

--- js/swipe_detect.js
+++ js/swipe_detect.js
@@ -2,13 +2,15 @@
 var hammer = new Hammer(projectCarousel);
 
 // Listen for swipeleft and swiperight events
-hammer.on('swipeleft', function() {
+hammer.on('swipeleft', function(ev) {
+  ev.preventDefault();
   // Trigger the next slide event
   // $(projectCarousel).carousel('next');
   CarouselButtonClicked(1);
 });
 
-hammer.on('swiperight', function() {
+hammer.on('swiperight', function(ev) {
+  ev.preventDefault();
   // Trigger the previous slide event
   // $(projectCarousel).carousel('prev');
   CarouselButtonClicked(-1);
