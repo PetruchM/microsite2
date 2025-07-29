0e5327ad2813e62a6a034582b4c6af3d4166cf18
Martin Petruch <78723403@cuni.cz>
2025-07-29 11:46:12 +0200

    basic cookies functioning

--- js/global.js
+++ js/global.js
@@ -11,33 +11,3 @@ function setVh() {
 updatePcOrMobile();
 
 window.addEventListener('resize', updatePcOrMobile);
-
-
-//cookies functioning for now
-function acceptCookies() {
-    //na 1 rok
-    const date = new Date();
-    date.setFullYear(date.getFullYear() + 1);
-    document.cookie = "cookie_consent=true; expires=" + date.toUTCString() + "; path=/";
-
-    // schovej banner
-    document.getElementById('cookie-banner').style.display = 'none';
-
-    loadGoogleAnalytics();
-}
-
-if (document.cookie.includes('cookie_consent=true')) {
-    document.getElementById('cookie-banner').style.display = 'none';
-    loadGoogleAnalytics();
-}
-
-function loadGoogleAnalytics() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
-        a=s.createElement(o), m=s.getElementsByTagName(o)[0];
-        a.async=1; a.src=g; m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-XXXXXXXXX-Y', 'auto'); // Vojta mi musí dát svůj kód
-    ga('send', 'pageview');
-}
